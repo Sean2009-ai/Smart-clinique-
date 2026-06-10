4b86b187e4a89cec5bfffd0a789784f98fcfbf4a
Sean2009-ai <edwinsean092@gmail.com>
2026-06-10 23:26:44 +0000

    Update app.py

--- app.py
+++ app.py
@@ -298,4 +298,4 @@ if __name__ == "__main__":
     init_db()
     print("🚀 SmartClinique démarré sur http://localhost:5000")
     app.run(host="0.0.0.0", port=5000, debug=True)
-    
+                 
